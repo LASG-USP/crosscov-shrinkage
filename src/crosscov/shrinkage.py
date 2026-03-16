761cabf7d5132d3c9e4391685bc258eef73cbbf1
Paulo Ranazzi <94863642+ranazzi@users.noreply.github.com>
2026-03-16 17:03:00 -0300

    Method implemented

--- src/crosscov/shrinkage.py
+++ src/crosscov/shrinkage.py
@@ -23,3 +23,23 @@ def shrink_cross_covariance(X, Y, ddof=0):
     C = cross_covariance(X, Y, ddof)
 
     return alpha* C
+
+
+def compute_localization_approximate(x, n, y=None, ddof=1):
+    n -= ddof
+    if type == 'full':
+        C = np.cov(x, ddof=ddof)
+        varx = np.diag(C)
+        vary = np.diag(C)
+    if y is not None:
+        C = cross_covariance(x, y)
+        varx = np.var(x, axis=-1, ddof=ddof)
+        vary = np.var(y, axis=-1, ddof=ddof)
+    P = np.zeros(C.shape)
+
+    for i in range(C.shape[0]):
+        for j in range(C.shape[1]):
+            phi = ((n/(n+2))**2*varx[i]*vary[j])/C[i, j] ** 2
+            P[i, j] = 0 if n < phi else (n-phi)/(n+1)
+
+    return P
